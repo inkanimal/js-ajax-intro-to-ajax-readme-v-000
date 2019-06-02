dd036cb112bb07b67372b463481ab1ba741786a5
geoffrey woodcock <theatomproject@gmail.com>
2019-06-02 21:18:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -20,3 +20,12 @@ function showRepositories() {
   repoList += '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
