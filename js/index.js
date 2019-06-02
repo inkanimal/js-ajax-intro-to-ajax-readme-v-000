edb661278e5c8895de8e68f11396cb132a1b9615
geoffrey woodcock <theatomproject@gmail.com>
2019-06-02 21:17:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,3 +9,14 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+
+function showRepositories() {
+  console.log(this.responseText);
+  let repoList = '<ul>';
+  for (var i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
